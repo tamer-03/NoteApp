7bfe182ba7d8c891cf50af593d44dcb6540f2abc
tamer-03 <tamerkndl.03@gmail.com>
2024-10-06 17:04:48 +0300

    add_note_page added

---
+++ lib/screen/add_note.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNote extends StatefulWidget {
+  const AddNote({super.key});
+
+  @override
+  State<AddNote> createState() => _AddNoteState();
+}
+
+class _AddNoteState extends State<AddNote> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
